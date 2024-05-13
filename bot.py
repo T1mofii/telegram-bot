657592ab77050add79baa19fe9b2b7ea524bbe3d
Timofii <tisenkot268@gmail.com>
2024-05-13 22:28:24 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,28 @@
+import os
+from flask import Flask, request
 import telebot
 
-bot = telebot.TeleBot('7124726550:AAG1fyj4esn9uACTiaLz1C0X5BZnCUFykpM')
+# Устанавливаем токен вашего бота
+TOKEN = '7124726550:AAG1fyj4esn9uACTiaLz1C0X5BZnCUFykpM'
 
+# Создаем объект бота
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+
+# Замените 'YOUR_DOMAIN' на ваше доменное имя сервера
+WEBHOOK_URL = f'https://telegram-bot-rurchik-4ac0317f5b9d.herokuapp.com/{TOKEN}'
+
+# Обработчик для вебхука
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+# Установка вебхука
+bot.remove_webhook()
+bot.set_webhook(url=WEBHOOK_URL)
+
+# Обработчики команд и сообщений вашего бота
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Ты замерз?")
@@ -34,4 +55,6 @@ def send_stickers(message):
     except ValueError:
         bot.send_message(message.chat.id, "Пожалуйста, введите число.")
 
-bot.polling(non_stop=True)
+# Запуск сервера Flask
+if __name__ == '__main__':
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
